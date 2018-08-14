e4fb228e3e490c03c4083e3f77752ee1d24cd1b1
An Phan <an.phan@internations.org>
2018-08-14 20:19:19 +0200

    Add ability to zoom in/out lyrics

--- js/components/main-wrapper/extra/lyrics.vue
+++ js/components/main-wrapper/extra/lyrics.vue
@@ -1,19 +1,57 @@
 <template>
   <article id="lyrics">
     <div class="content">
-      <div v-if="song.lyrics" v-html="song.lyrics"></div>
+      <template v-if="song.lyrics">
+        <div ref="lyricsContainer" v-html="song.lyrics"></div>
+        <text-zoomer :target="textZoomTarget"/>
+      </template>
       <p class="none" v-if="song.id && !song.lyrics">No lyrics found. Are you not listening to Bach?</p>
     </div>
   </article>
 </template>
 
 <script>
+import TextZoomer from '@/components/shared/text-zoomer.vue'
+
 export default {
   props: {
     song: {
       type: Object,
       required: true
     }
+  },
+
+  components: { TextZoomer },
+
+  data: () => ({
+    textZoomTarget: null
+  }),
+
+  mounted () {
+    // Since Vue's $refs are not reactive, we workaround by assigning to a data property
+    this.textZoomTarget = this.$refs.lyricsContainer
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.content {
+  line-height: 1.6;
+  position: relative;
+
+  .text-zoomer {
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  &:hover .text-zoomer {
+    opacity: .5;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
---
+++ js/components/shared/text-zoomer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="text-zoomer">
+    <button @click.prevent="zoom(-1)"><i class="fa fa-search-minus"></i></button>
+    <button @click.prevent="zoom(1)"><i class="fa fa-search-plus"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    target: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    zoom (multiplier) {
+      const style = this.target.style
+
+      if (style.fontSize === '') {
+        style.fontSize = '1em'
+        style.lineHeight = 1.6
+      }
+
+      style.fontSize = parseFloat(style.fontSize) + multiplier * 0.2 + 'em'
+      style.lineHeight = parseFloat(style.lineHeight) + multiplier * 0.15
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~#/partials/_vars";
+@import "~#/partials/_mixins";
+
+.text-zoomer {
+  display: flex;
+  transition: .2s;
+
+  button {
+    @include inset-when-pressed();
+
+    background: $colorExtraBgr;
+    border: 1px solid $colorGrey;
+    color: rgba(255, 255, 255, .5);
+    transition: background .2s;
+
+    &:hover {
+      background: $colorGrey;
+      color: #fff;
+    }
+
+    &:first-of-type {
+      border-radius: 4px 0 0 4px;
+      border-right: 0;
+    }
+
+    &:last-of-type {
+      border-radius: 0 4px 4px 0;
+    }
+  }
+}
+</style>
