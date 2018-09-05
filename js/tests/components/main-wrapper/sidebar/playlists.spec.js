5ee95a5909aa743d1b61b6d1946af49be31d9867
Phan An <me@phanan.net>
2018-09-05 19:42:03 +0700

    Fix playlists test

--- js/tests/components/main-wrapper/sidebar/playlists.spec.js
+++ js/tests/components/main-wrapper/sidebar/playlists.spec.js
@@ -2,13 +2,19 @@ import Component from '@/components/main-wrapper/sidebar/playlists.vue'
 import PlaylistItem from '@/components/main-wrapper/sidebar/playlist-item.vue'
 import factory from '@/tests/factory'
 
-describe('compopents/main-wrapper/main-content/sidebar/playlist', () => {
-  it('renders properly', () => {
-    const wrapper = mount(Component, { data: {
-      playlistState: {
-        playlists: factory('playlist', 5)
-      }
-    }})
-    wrapper.findAll(PlaylistItem).should.have.lengthOf(6) // favorites + 5 playlists
+describe('compopents/main-wrapper/main-content/sidebar/playlists', () => {
+  it('renders properly', async (done) => {
+    const wrapper = await mount(Component, {
+      data: () => ({
+        playlistState: {
+          playlists: factory('playlist', 5)
+        }
+      })
+    })
+
+    wrapper.vm.$nextTick(() => {
+      wrapper.findAll(PlaylistItem).should.have.lengthOf(6) // favorites + 5 playlists
+      done()
+    })
   })
 })
