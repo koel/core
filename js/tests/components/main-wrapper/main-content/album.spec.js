54962bb8399ed39a84d07454ed616a2d2d9474fc
Phan An <me@phanan.net>
2018-09-06 09:00:26 +0700

    Fix album tests

--- js/tests/components/main-wrapper/main-content/album.spec.js
+++ js/tests/components/main-wrapper/main-content/album.spec.js
@@ -1,35 +1,28 @@
 import Component from '@/components/main-wrapper/main-content/album.vue'
 import SongList from '@/components/shared/song-list.vue'
 import SongListControls from '@/components/shared/song-list-controls.vue'
-import { event } from '@/utils'
 import { download, albumInfo as albumInfoService } from '@/services'
 import factory from '@/tests/factory'
 
 describe('components/main-wrapper/main-content/album', () => {
-  it('renders upon receiving event', () => {
-    const wrapper = shallow(Component)
-    // stub the $nextTick so that the sort() method is not called
-    // on non-existing $refs.songList
-    const nextTickStub = sinon.stub(wrapper.vm, '$nextTick')
+  it('renders properly', () => {
     const album = factory('album')
-    event.emit(event.$names.LOAD_MAIN_CONTENT, 'album', album)
-    Vue.nextTick(() => {
-      const html = wrapper.html()
-      html.should.contain(album.name)
-      html.should.contain(album.artist.name)
-      wrapper.hasAll(SongList, SongListControls).should.be.true
-
-      nextTickStub.restore()
+    const wrapper = shallow(Component, {
+      propsData: { album }
     })
+    const html = wrapper.html()
+    html.should.contain(album.name)
+    html.should.contain(album.artist.name)
+    wrapper.hasAll(SongList, SongListControls).should.be.true
   })
 
   it('loads info from Last.fm', () => {
     const album = factory('album', { info: null })
     const wrapper = shallow(Component, {
-      data: {
-        album,
+      propsData: { album },
+      data: () => ({
         sharedState: { useLastfm: true }
-      }
+      })
     })
     const stub = sinon.stub(albumInfoService, 'fetch')
     wrapper.click('a.info')
@@ -40,10 +33,10 @@ describe('components/main-wrapper/main-content/album', () => {
   it('allows downloading', () => {
     const album = factory('album')
     const wrapper = shallow(Component, {
-      data: {
-        album,
+      propsData: { album },
+      data: () => ({
         sharedState: { allowDownload: true }
-      }
+      })
     })
     const downloadStub = sinon.stub(download, 'fromAlbum')
     wrapper.click('a.download')
