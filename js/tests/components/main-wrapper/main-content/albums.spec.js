68ed7a4a592f77f784839080e2bfca7c15a74c51
Phan An <me@phanan.net>
2018-09-06 09:02:36 +0700

    Fix albums tests

--- js/tests/components/main-wrapper/main-content/albums.spec.js
+++ js/tests/components/main-wrapper/main-content/albums.spec.js
@@ -3,9 +3,16 @@ import AlbumItem from '@/components/shared/album-item.vue'
 import factory from '@/tests/factory'
 
 describe('components/main-wrapper/main-content/albums', () => {
-  it('displays a list of albums', () => {
-    shallow(Albums, { data: {
-      albums: factory('album', 5)
-    }}).findAll(AlbumItem).should.have.lengthOf(5)
+  it('displays a list of albums', async (done) => {
+    const wrapper = await mount(Albums, {
+      data: () => ({
+        albums: factory('album', 5)
+      })
+    })
+
+    Vue.nextTick(() => {
+      wrapper.findAll(AlbumItem).should.have.lengthOf(5)
+      done()
+    })
   })
 })
