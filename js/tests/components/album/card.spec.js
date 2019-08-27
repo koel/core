44d7985b9130bf242d5a212ea8b96fa65fb71c95
Phan An <me@phanan.net>
2019-08-27 07:53:35 +0200

    Add and update tests

--- js/tests/components/album/card.spec.js
+++ js/tests/components/album/card.spec.js
@@ -1,20 +1,18 @@
-import each from 'jest-each'
-import Component from '@/components/album/card.vue'
+import Component from '@/components/album/card'
+import Thumbnail from '@/components/ui/album-artist-thumbnail'
 import factory from '@/tests/factory'
 import { playback, download } from '@/services'
-import { queueStore, sharedStore } from '@/stores'
+import { sharedStore } from '@/stores'
 import { mock } from '@/tests/__helpers__'
 
 describe('components/album/card', () => {
   let album
-  let wrapper
 
   beforeEach(() => {
     album = factory('album', {
       songs: factory('song', 10)
     })
     sharedStore.state = { allowDownload: true }
-    wrapper = shallow(Component, { propsData: { album }})
   })
 
   afterEach(() => {
@@ -22,33 +20,31 @@ describe('components/album/card', () => {
     jest.clearAllMocks()
   })
 
-  it('renders properly', () => {
-    expect(wrapper.has('span.cover')).toBe(true)
-    const html = wrapper.html()
-    expect(html).toMatch(album.name)
-    expect(html).toMatch('10 songs')
-  })
+  it('renders properly', async done => {
+    const wrapper = await mount(Component, { propsData: { album }})
 
-  it('plays if clicked', () => {
-    const m = mock(playback, 'playAllInAlbum')
-    wrapper.click('.control-play')
-    expect(m).toHaveBeenCalledWith(album, false)
-  })
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.has(Thumbnail)).toBe(true)
+      const html = wrapper.html()
+      expect(html).toMatch(album.name)
+      expect(html).toMatch('10 songs')
 
-  each([['metaKey'], ['ctrlKey']]).test('queues if %s is clicked', key => {
-    const m = mock(queueStore, 'queue')
-    wrapper.click('.control-play', { [key]: true })
-    expect(m).toHaveBeenCalled()
+      done()
+    })
   })
 
   it('shuffles', () => {
+    const wrapper = shallow(Component, { propsData: { album }})
     const m = mock(playback, 'playAllInAlbum')
+
     wrapper.click('.shuffle-album')
     expect(m).toHaveBeenCalledWith(album, true)
   })
 
   it('downloads', () => {
+    const wrapper = shallow(Component, { propsData: { album }})
     const m = mock(download, 'fromAlbum')
+
     wrapper.click('.download-album')
     expect(m).toHaveBeenCalledWith(album)
   })
--- js/tests/components/artist/card.spec.js
+++ js/tests/components/artist/card.spec.js
@@ -1,13 +1,12 @@
-import each from 'jest-each'
 import Component from '@/components/artist/card.vue'
+import Thumbnail from '@/components/ui/album-artist-thumbnail'
 import factory from '@/tests/factory'
 import { playback, download } from '@/services'
-import { queueStore, sharedStore } from '@/stores'
+import { sharedStore } from '@/stores'
 import { mock } from '@/tests/__helpers__'
 
 describe('components/artist/card', () => {
   let artist
-  let wrapper
 
   beforeEach(() => {
     sharedStore.state = { allowDownload: true }
@@ -16,7 +15,6 @@ describe('components/artist/card', () => {
       albums: factory('album', 4),
       songs: factory('song', 16)
     })
-    wrapper = shallow(Component, { propsData: { artist }})
   })
 
   afterEach(() => {
@@ -24,34 +22,32 @@ describe('components/artist/card', () => {
     jest.clearAllMocks()
   })
 
-  it('renders properly', () => {
-    expect(wrapper.has('span.cover')).toBe(true)
-    const html = wrapper.html()
-    expect(html).toMatch('4 albums')
-    expect(html).toMatch('16 songs')
-    expect(html).toMatch(artist.name)
-  })
+  it('renders properly', async done => {
+    const wrapper = await mount(Component, { propsData: { artist }})
 
-  it('plays if clicked', () => {
-    const playStub = mock(playback, 'playAllByArtist')
-    wrapper.click('.control-play')
-    expect(playStub).toHaveBeenCalledWith(artist, false)
-  })
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.has(Thumbnail)).toBe(true)
+      const html = wrapper.html()
+      expect(html).toMatch('4 albums')
+      expect(html).toMatch('16 songs')
+      expect(html).toMatch(artist.name)
 
-  each([['metaKey'], ['ctrlKey']]).test('queues if %s is clicked', key => {
-    const m = mock(queueStore, 'queue')
-    wrapper.click('.control-play', { [key]: true })
-    expect(m).toHaveBeenCalled()
+      done()
+    })
   })
 
   it('shuffles', () => {
+    const wrapper = shallow(Component, { propsData: { artist }})
     const playStub = mock(playback, 'playAllByArtist')
+
     wrapper.click('.shuffle-artist')
     expect(playStub).toHaveBeenCalledWith(artist, true)
   })
 
   it('downloads', () => {
+    const wrapper = shallow(Component, { propsData: { artist }})
     const downloadStub = mock(download, 'fromArtist')
+
     wrapper.click('.download-artist')
     expect(downloadStub).toHaveBeenCalledWith(artist)
   })
