e8db063d6da4331fad0ebfc5ca1a6c5683bf984c
Phan An <me@phanan.net>
2019-10-23 15:31:57 +0200

    chore: improve ls and its tests

--- js/services/ls.js
+++ js/services/ls.js
@@ -1,7 +1,11 @@
 import localStorage from 'local-storage'
 
 export const ls = {
-  get: (key, defaultVal = null) => localStorage(key) || defaultVal,
-  set: (key, val) => localStorage(key, val),
+  get: (key, defaultValue = null) => {
+    const value = localStorage.get(key)
+
+    return value === null ? defaultValue : value
+  },
+  set: (key, value) => localStorage.set(key, value),
   remove: key => localStorage.remove(key)
 }
---
+++ js/tests/__mocks__/local-storage.js
@@ -0,0 +1,5 @@
+module.exports = {
+  get: jest.fn(key => null),
+  set: jest.fn((key, val) => {}),
+  remove: jest.fn(key => {})
+}
