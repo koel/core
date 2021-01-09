c6c7604ba9928da1f9cafbc964d215e8c6bdb299
Phan An <me@phanan.net>
2021-01-09 11:35:35 +0100

    fix: show play/pause button properly

--- js/components/layout/app-footer/player-controls.vue
+++ js/components/layout/app-footer/player-controls.vue
@@ -64,7 +64,7 @@ export default Vue.extend({
     },
 
     shouldDisplayPlayButton (): boolean {
-      return !this.song || (this.song && this.song.playbackState === 'Playing')
+      return !this.song || (this.song && this.song.playbackState !== 'Playing')
     }
   },
 
